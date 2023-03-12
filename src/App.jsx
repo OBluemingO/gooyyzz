c6ae3f85cc4c848d04e464fe6713d2ede8a13d45
phayuphat trilao <develop_phayuphat.t@xsurface.com>
2023-03-12 17:55:06 +0700

    remove cursor pointer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/bars/navbar";
 import ContactUs from "./components/bars/contactUs";
 import { useFollowPointer } from "./hooks/useFollowPointer";
 import Home from "./pages/home";
-import { useRef, useState, useContext } from "react";
+import { useRef, useState, useContext, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Context } from "./store";
 
@@ -12,12 +12,12 @@ function App() {
     const {
         cursor,
         cursorDown,
-        pointer: { x, y },
+        pointer: { x, y }
     } = useContext(Context);
 
     return (
         <div className="bg-light-purple max-w-[1440px] mx-auto">
-            {/* <Navbar /> */}
+            <Navbar />
             <motion.div
                 className="w-[50px] h-[50px] rounded-full bg-[#fd7702] fixed z-[500] pointer-events-none"
                 ref={ref}
--- src/components/bars/navbar.jsx
+++ src/components/bars/navbar.jsx
@@ -6,24 +6,24 @@ const Navbar = () => {
     return (
         <div className="z-[300] bg-light-purple text-white flex justify-around items-center fixed w-full h-full max-w-[1440px] lg:max-h-[130px]">
             <div className="flex gap-20">
-                <div className="text-2xl cursor-pointer">Home</div>
-                <div className="text-2xl cursor-pointer">Shop</div>
+                <div className="text-2xl">Home</div>
+                <div className="text-2xl">Shop</div>
             </div>
             <div className="flex items-center">
                 <div className="flex gap-10">
-                    <div className="text-2xl cursor-pointer">Categories</div>
-                    <div className="text-2xl cursor-pointer">About</div>
+                    <div className="text-2xl">Categories</div>
+                    <div className="text-2xl">About</div>
                 </div>
                 <div className="flex justify-center gap-5 lg:w-[150px]">
-                    <div className="cursor-pointer">
+                    <div className="">
                         <AiOutlineSearch size={20} />
                     </div>
-                    <div className="cursor-pointer">
+                    <div className="">
                         <BiShoppingBag size={20} />
                     </div>
                 </div>
             </div>
-            <div className="font-light absolute left-1/2 -translate-x-1/2 text-5xl cursor-pointer">ULTIMATE</div>
+            <div className="font-light absolute left-1/2 -translate-x-1/2 text-5xl">ULTIMATE</div>
             <div className="border-b-[1px] w-full absolute bottom-0"></div>
         </div>
     );
